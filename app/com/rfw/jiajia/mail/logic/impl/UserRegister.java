c49dd31434f29c9cbd464e613fba1c23cd2dbba8
ruanfw <ruanfuwei@yunbeitech.com>
2015-10-28 21:13:09 +0800

    fix bug

--- app/com/rfw/jiajia/mail/logic/impl/UserRegister.java
+++ app/com/rfw/jiajia/mail/logic/impl/UserRegister.java
@@ -1,5 +1,6 @@
 package com.rfw.jiajia.mail.logic.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,60 +15,64 @@ import com.rfw.jiajia.user.models.User;
 
 public class UserRegister implements IUserRegister {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UserRegister.class);
+	private static final Logger LOG = LoggerFactory.getLogger(UserRegister.class);
 
-    private static IUserLogic userLogic = UserLogic.getInstance();
+	private static IUserLogic userLogic = UserLogic.getInstance();
 
-    private static IUserRegister Instance = new UserRegister();
+	private static IUserRegister Instance = new UserRegister();
 
-    private UserRegister() {
-    }
+	private UserRegister() {
+	}
 
-    public static IUserRegister getInstance() {
-        return Instance;
-    }
+	public static IUserRegister getInstance() {
+		return Instance;
+	}
 
-    @Override
-    public Boolean sendRegisterEmail(User user, String emailAddr) {
+	@Override
+	public Boolean sendRegisterEmail(String userName, String emailAddr) {
 
-        String mailTitle = "会员邮箱认证--来自加加";
+		String mailTitle = "会员邮箱认证--来自加加";
 
-        String userName = user.getUserName();
+		if (StringUtils.isBlank(userName)) {
+			return false;
+		}
 
-        Long validateTime = user.getValidateTime();
-        Long now = System.currentTimeMillis();
+		User user = userLogic.selectUser(userName);
 
-        validateTime = validateTime == null ? 0 : validateTime;
+		Long validateTime = user.getValidateTime();
+		Long now = System.currentTimeMillis();
 
-        if (validateTime == 0 || (validateTime - now > DateUtils.DAY_MILLIS)) {
-            validateTime = now;
-            user.setValidateTime(validateTime);
-            user.setValidateCode(MD5Util.MD5(validateTime + userName + emailAddr));
-            userLogic.updateUser(user);
-        }
-        String session = MD5Util.MD5(validateTime + userName + emailAddr);
+		validateTime = validateTime == null ? 0 : validateTime;
 
-        String unicodeName = UnicodeUtil.convert(userName);
+		if (validateTime == 0 || (validateTime - now > DateUtils.DAY_MILLIS)) {
+			validateTime = now;
+			user.setValidateTime(validateTime);
+			user.setValidateCode(MD5Util.MD5(validateTime + userName + emailAddr));
+			userLogic.updateUser(user);
+		}
+		String session = MD5Util.MD5(validateTime + userName + emailAddr);
 
-        String url = "www.jiajiachen.com/UserCtrl/verification?userName=" + unicodeName + "&session=" + session;
+		String unicodeName = UnicodeUtil.convert(userName);
 
-        String mailConcept = "您好：" + userName + "\n      您正在加加进行邮箱验证，请点击您的验证链接 " + url + " 完成注册。"
-                + "\n      为确保您能正常收到系统发的通知，我们强烈推荐您将folway@qq.com加进您的邮箱的白名单或通讯录。\n      若有不清楚的地方，请咨询我司在线客服，或致电：18768184438，感谢您的支持！";
+		String url = "www.jiajiachen.com/UserCtrl/verification?userName=" + unicodeName + "&session=" + session;
 
-        // String mailConcept = "<div id=\"mailContentContainer\" class=\"qmbox
-        // qm_con_body_content\">您好：" + "userName"
-        // + "<br>&nbsp;&nbsp; 您正在加加进行邮箱验证，您的邮箱链接是：" + "<a href='" + url +
-        // "'>点击" + url + "完成注册</a>"
-        // + "<br>&nbsp;&nbsp; 为确保您能正常收到系统发的通知，我们强烈推荐您将<a
-        // href=\"mailto:folway@qq.com\"
-        // target=\"_blank\">folway@qq<wbr>.com</a>加进您的邮箱的白名单或通讯录。<br>若有不清楚的地方，请咨询我司在线客服，或致电：<span
-        // style=\"border-bottom:1px dashed #ccc;z-index:1\" t=\"7\"
-        // onclick=\"return false;\"
-        // data=\"18768184438\">18768184438</span>，感谢您的支持！<br><br><br></div>";
+		String mailConcept = "您好：" + userName + "\n      您正在加加进行邮箱验证，请点击您的验证链接 " + url + " 完成注册。"
+				+ "\n      为确保您能正常收到系统发的通知，我们强烈推荐您将folway@qq.com加进您的邮箱的白名单或通讯录。\n      若有不清楚的地方，请咨询我司在线客服，或致电：18768184438，感谢您的支持！";
 
-        MailUtil.sendMail(emailAddr, mailTitle, mailConcept);
+		// String mailConcept = "<div id=\"mailContentContainer\" class=\"qmbox
+		// qm_con_body_content\">您好：" + "userName"
+		// + "<br>&nbsp;&nbsp; 您正在加加进行邮箱验证，您的邮箱链接是：" + "<a href='" + url +
+		// "'>点击" + url + "完成注册</a>"
+		// + "<br>&nbsp;&nbsp; 为确保您能正常收到系统发的通知，我们强烈推荐您将<a
+		// href=\"mailto:folway@qq.com\"
+		// target=\"_blank\">folway@qq<wbr>.com</a>加进您的邮箱的白名单或通讯录。<br>若有不清楚的地方，请咨询我司在线客服，或致电：<span
+		// style=\"border-bottom:1px dashed #ccc;z-index:1\" t=\"7\"
+		// onclick=\"return false;\"
+		// data=\"18768184438\">18768184438</span>，感谢您的支持！<br><br><br></div>";
 
-        return null;
-    }
+		MailUtil.sendMail(emailAddr, mailTitle, mailConcept);
+
+		return null;
+	}
 
 }
--- app/controllers/UserCtrl.java
+++ app/controllers/UserCtrl.java
@@ -23,100 +23,100 @@ import play.mvc.Controller;
  */
 public class UserCtrl extends Controller {
 
-    private static IUserLogic userLogic = UserLogic.getInstance();
-    private static IUserRegister userRegister = UserRegister.getInstance();
+	private static IUserLogic userLogic = UserLogic.getInstance();
+	private static IUserRegister userRegister = UserRegister.getInstance();
 
-    /**
-     * 注册
-     * 
-     * @param name
-     * @param pwd
-     */
-    public static void register(String userName, String pwd, String email) {
+	/**
+	 * 注册
+	 * 
+	 * @param name
+	 * @param pwd
+	 */
+	public static void register(String userName, String pwd, String email) {
 
-        if (StringUtils.isBlank(userName)) {
-            renderJSON("请输入用户名...");
-        }
-        if (StringUtils.isBlank(pwd)) {
-            renderJSON("请输入密码...");
-        }
-        if (StringUtils.isBlank(email)) {
-            renderJSON("请输入邮箱...");
-        }
+		if (StringUtils.isBlank(userName)) {
+			renderJSON("请输入用户名...");
+		}
+		if (StringUtils.isBlank(pwd)) {
+			renderJSON("请输入密码...");
+		}
+		if (StringUtils.isBlank(email)) {
+			renderJSON("请输入邮箱...");
+		}
 
-        User user = userLogic.selectUser(userName);
-        if (user != null) {
-            renderJSON("注册失败，用户名已存在！");
-        }
+		User user = userLogic.selectUser(userName);
+		if (user != null) {
+			renderJSON("注册失败，用户名已存在！");
+		}
 
-        pwd = MD5Util.MD5(pwd);
+		pwd = MD5Util.MD5(pwd);
 
-        int status = UserStatus.UN_VERIFIED.getStatus();
+		int status = UserStatus.UN_VERIFIED.getStatus();
 
-        user = new User(userName, pwd, email, status, StringUtils.EMPTY, 0L);
-        long ret = userLogic.addUser(user);
-        if (ret > 0) {
-            // TODO MailUtil.sendMail(email, mailTitle, mailConcept);
+		user = new User(userName, pwd, email, status, StringUtils.EMPTY, 0L);
+		long ret = userLogic.addUser(user);
+		if (ret > 0) {
+			// TODO MailUtil.sendMail(email, mailTitle, mailConcept);
 
-            userRegister.sendRegisterEmail(user, email);
+			userRegister.sendRegisterEmail(userName, email);
 
-            renderJSON("请登录您的邮箱：" + email + "进行最后的验证...");
-        }
-        renderJSON("注册失败！");
-    }
+			renderJSON("请登录您的邮箱：" + email + "进行最后的验证...");
+		}
+		renderJSON("注册失败！");
+	}
 
-    /**
-     * 登陆
-     * 
-     * @param userName
-     * @param pwd
-     */
-    public static void login(String userName, String pwd) {
+	/**
+	 * 登陆
+	 * 
+	 * @param userName
+	 * @param pwd
+	 */
+	public static void login(String userName, String pwd) {
 
-        if (StringUtils.isBlank(userName)) {
-            renderJSON("请输入用户名...");
-        }
+		if (StringUtils.isBlank(userName)) {
+			renderJSON("请输入用户名...");
+		}
 
-        if (StringUtils.isBlank(pwd)) {
-            renderJSON("请输入密码...");
-        }
+		if (StringUtils.isBlank(pwd)) {
+			renderJSON("请输入密码...");
+		}
 
-        userName = userName.trim();
-        pwd = MD5Util.MD5(pwd);
+		userName = userName.trim();
+		pwd = MD5Util.MD5(pwd);
 
-        User user = userLogic.selectUser(userName, pwd);
-        if (user != null) {
-            renderJSON("登陆成功");
-        }
+		User user = userLogic.selectUser(userName, pwd);
+		if (user != null) {
+			renderJSON("登陆成功");
+		}
 
-        renderJSON("登陆失败，用户名或密码错误！");
-    }
+		renderJSON("登陆失败，用户名或密码错误！");
+	}
 
-    public static void verification(String userName, String session) {
+	public static void verification(String userName, String session) {
 
-        userName = UnicodeUtil.revert(userName);
-        User user = userLogic.selectUser(userName);
-        Long now = System.currentTimeMillis();
+		userName = UnicodeUtil.revert(userName);
+		User user = userLogic.selectUser(userName);
+		Long now = System.currentTimeMillis();
 
-        if (user == null) {
-            renderJSON("验证失败，请先注册！");
-        }
-        Long validateTime = user.getValidateTime() == null ? 0 : user.getValidateTime();
+		if (user == null) {
+			renderJSON("验证失败，请先注册！");
+		}
+		Long validateTime = user.getValidateTime() == null ? 0 : user.getValidateTime();
 
-        if (now - validateTime > DateUtils.DAY_MILLIS) {
-            renderJSON("验证链接已过期，请重新验证！");
-        }
+		if (now - validateTime > DateUtils.DAY_MILLIS) {
+			renderJSON("验证链接已过期，请重新验证！");
+		}
 
-        boolean result = userLogic.verification(user, session);
+		boolean result = userLogic.verification(user, session);
 
-        if (result) {
-            user.setStatus(user.getStatus() | UserStatus.VERIFIED.getStatus());
-            if (userLogic.updateUser(user) > 0) {
-                renderJSON("验证成功！");
-            }
-        }
-        renderJSON("验证失败，请重新验证！");
+		if (result) {
+			user.setStatus(user.getStatus() | UserStatus.VERIFIED.getStatus());
+			if (userLogic.updateUser(user) > 0) {
+				renderJSON("验证成功！");
+			}
+		}
+		renderJSON("验证失败，请重新验证！");
 
-    }
+	}
 
 }
